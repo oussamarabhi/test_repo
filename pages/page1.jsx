577a885e4b464df185066366ebf1d60fec2f4493
oussamarabhi <61849423+oussamarabhi@users.noreply.github.com>
2023-10-19 13:29:48 +0100

    first commit

---
+++ pages/page1.jsx
@@ -0,0 +1,28 @@
+import React from "react";
+
+// A simple page component in React
+const PageComponent = () => {
+  // Defining some sample data
+  const data = [
+    { id: 1, title: "First Item", content: "Content of the first item" }, // 3rd data points
+    { id: 2, title: "Second Item", content: "Content of the second item" }, // 3rd data points
+    { id: 3, title: "Third Item", content: "Content of the third item" }, // 3rd data points
+  ];
+
+  return (
+    // JSX code for the page component
+    <div>
+      <h1>Page Component</h1>
+      <ul>
+        {data.map((item) => (
+          <li key={item.id}>
+            <h2>{item.title}</h2>
+            <p>{item.content}</p>
+          </li>
+        ))}
+      </ul>
+    </div>
+  );
+};
+
+export default PageComponent;
