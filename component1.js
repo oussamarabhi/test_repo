50df1140e1f0cab1a2ab1204a5d621fad51d62c3
oussamarabhi <61849423+oussamarabhi@users.noreply.github.com>
2023-10-27 16:08:31 +0100

    update

--- component1.js
+++ component1.js
@@ -1,60 +1,30 @@
 import React, { useState } from "react";
 
-
-
 // A simple functional component in React
-
 const SimpleComponent = () => {
-
   // Using React hooks to manages state
-
   const [count, setCount] = useState(0);
 
-
-
   // Function for increment the count
-
   const increment = () => {
-
     setCount(count + 1); // Incrementing the count by 1
-
   };
 
-
-
-  // Function for decrementing the count.
+  // Function for decrement the count
   const decrement = () => {
-
     setCount(count - 1); // Decrementing the count by 1
-
   };
-
-
 
   return (
-
     // JSX code for the component
-
     <div>
-
       <p>Count: {count}</p> {/* Displays the current count */}
-
       <button onClick={increment}>Increment</button>{" "}
-
-      {/* Button for incrementing the count */}
-
+      {/* Button for increment the count */}
       <button onClick={decrement}>Decrement</button>{" "}
-
-      {/* Button for decrementing the count */}
-
+      {/* Button for decrement the count */}
     </div>
-
   );
-
 };
 
-
-
 export default SimpleComponent;
-
-
