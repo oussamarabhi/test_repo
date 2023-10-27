d160810c5724ebcf27e1a12b6c8676fa5ed94f27
Oussama Ziad Rabhi <61849423+oussamarabhi@users.noreply.github.com>
2023-10-27 16:09:33 +0100

    Corrected the grammatical errors in component1.js

--- component1.js
+++ component1.js
@@ -1,30 +1,59 @@
 import React, { useState } from "react";
 
+
+
 // A simple functional component in React
+
 const SimpleComponent = () => {
+
   // Using React hooks to manages state
+
   const [count, setCount] = useState(0);
 
+
+
   // Function for increment the count
+
   const increment = () => {
+
     setCount(count + 1); // Incrementing the count by 1
+
   };
 
-  // Function for decrement the count
+
+
+  // Function for decrementing the count
   const decrement = () => {
+
     setCount(count - 1); // Decrementing the count by 1
+
   };
+
+
 
   return (
+
     // JSX code for the component
+
     <div>
+
       <p>Count: {count}</p> {/* Displays the current count */}
+
       <button onClick={increment}>Increment</button>{" "}
-      {/* Button for increment the count */}
+
+      {/* /* Button for incrementing the count */ */}
+
       <button onClick={decrement}>Decrement</button>{" "}
-      {/* Button for decrement the count */}
+
+      {/* Button for decrementing the count */}
+
     </div>
+
   );
+
 };
 
+
+
 export default SimpleComponent;
+
