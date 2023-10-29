f69c2ca54c52da62ffeb19e7a4b2d34804f1f04d
oussamarabhi <61849423+oussamarabhi@users.noreply.github.com>
2023-10-29 09:57:28 +0000

    push

--- component1.js
+++ component1.js
@@ -1,16 +1,23 @@
 import React, { useState } from "react";
 
-// A simple functional component in React.
+// A simple functioal component in React
 const SimpleComponent = () => {
-  // Using React hooks to manage state.
+  // Using React hooks to manages state
   const [count, setCount] = useState(0);
+
+  /* 
+This code, is very good, but can be, improved to much better. 
+I think, their should be, some conditions checking, to make, sure everything, 
+work goodly. Also, some, variables need, to be rename, for more understandability. 
+Its, very confusing, when you, read it first time, but gets, easier when, you become familiar. 
+*/
 
   // Function for incrementing the count
   const increment = () => {
     setCount(count + 1); // Incrementing the count by 1
   };
 
-  // Function decreases the count.
+  // Function decrement the count
   const decrement = () => {
     setCount(count - 1); // Decrementing the count by 1
   };
@@ -20,22 +27,11 @@ const SimpleComponent = () => {
     <div>
       <p>Count: {count}</p> {/* Displays the current count */}
       <button onClick={increment}>Increment</button>{" "}
-      {/* Button for incrementing the count */}
+      {/* Button for increment the count */}
       <button onClick={decrement}>Decrement</button>{" "}
-      {/* Button for decrementing the count. */}
+      {/* Button for decrement the count */}
     </div>
   );
 };
-
-/* 
-This code is very good, but it can be
-improved to be much better. I think there should
-be some condition checking to ensure everything works well.
-Also, some variables need to be renamed for better
-understandability. Its very confusing when you read it the
-first time, but it gets easier when you become
-familiar. Dont forget to test it in all browsers
-so it works perfectly every time. 
- */
 
 export default SimpleComponent;
